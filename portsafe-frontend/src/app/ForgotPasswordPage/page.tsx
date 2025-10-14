67ffaf96aca12771d8b1db57aa4c124ec00fd481
juliananascimento <estudos.juliananascimento@gmail.com>
2025-10-14 13:12:26 -0300

    :iphone: adjust the responsiveness of the Forgot Password Page PortSafee/Portsafee#26

--- portsafe-frontend/src/app/ForgotPasswordPage/page.tsx
+++ portsafe-frontend/src/app/ForgotPasswordPage/page.tsx
@@ -64,36 +64,37 @@ const ForgotPasswordPage: React.FC = () => {
       <div className="w-full max-w-[600px] min-w-[300px] bg-[#ffffff26] rounded-3xl text-white text-center mx-4 sm:mx-6 md:mx-auto">
 
         {/* Cabeçalho */}
-        <div className="flex items-center justify-between p-10 bg-[#084571] rounded-t-3xl min-h-[150px]">
-          <div>
+        <div className="flex flex-col sm:flex-row items-center justify-between p-10 bg-[#084571] rounded-t-3xl min-h-[150px] gap-4">
+          <div className="text-left sm:text-left">
             <h1 className="title font-marmelad text-2xl">Esqueceu a senha</h1>
             <h3>Recupere acesso à sua conta rapidamente</h3>
           </div>
           <img src={IconLogo.src} alt="Logo" className="w-[24%] max-w-[120px] min-w-[60px]" />
         </div>
 
-        {/* Campo */}
-        <div className="px-4 mb-3 sm:px-10 md:px-20">
-          <p className="text-left mt-4 text-lg pl-4">E-mail</p>
+        {/* Campo de email */}
+        <div className="px-4 sm:px-10 md:px-20 mt-6 mb-4">
+          <p className="text-left text-lg pl-4">E-mail</p>
           <Input
             placeholder="exemplo@email.com"
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className="w-full h-10 pl-4"
+            className="w-full h-10 pl-4 mt-1"
           />
+          <h2 className="text-gray-400 text-sm mt-4">
+            Enviaremos um código de verificação para o seu e-mail
+          </h2>
         </div>
-        <h2 className="text-gray-400 text-sm">
-          Enviaremos um código de verificação para o seu e-mail
-        </h2>
 
         {/* Botão */}
-        <Button 
-        nome="Enviar link de recuperação" 
-        estilo="secundary" 
-        clique={handleForgotPassword}
-        className='mt-3 mb-4'
+        <Button
+          nome="Redefinir"
+          estilo="secundary"
+          clique={handleForgotPassword}
+          className=' mb-6 px-4 w-full text-sm sm:text-base md:text-base truncate'
         />
+
       </div>
     </div>
   );
