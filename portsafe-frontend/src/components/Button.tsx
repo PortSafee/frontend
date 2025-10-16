3032312115cfb020c53316fed4178c5b02492bd7
juliananascimento <estudos.juliananascimento@gmail.com>
2025-10-15 22:34:27 -0300

    :lipstick: feat: adding new button styles

--- portsafe-frontend/src/components/Button.tsx
+++ portsafe-frontend/src/components/Button.tsx
@@ -1,17 +1,20 @@
 interface IBotao {
   nome: string;
-  estilo: "primary" | "secundary";
-  clique: () => void;
+  estilo: "primary" | "secundary" | "portaria" | "entregador";
+  clique?: () => void;
   className?: string;
+  icon?: React.ReactNode;
 }
 
-export default function Button({ nome, estilo, clique, className }: IBotao) {
+export default function Button({ nome, estilo, clique, className, icon }: IBotao) {
   return (
     <button
       onClick={clique}
-      className={`btn btn-${estilo} ${className ?? ""}`}
+       className={`flex items-center justify-center gap-2 btn btn-${estilo} ${className ?? ""}`}
     >
       {nome}
+      {icon && <span>{icon}</span>} {/* renderiza o ícone se existir */}
+
     </button>
   );
 }
