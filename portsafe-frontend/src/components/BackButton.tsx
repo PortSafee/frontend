f51387b3833a3b3edf301ee15cb020c0d99fc282
juliananascimento <estudos.juliananascimento@gmail.com>
2025-12-06 16:40:37 -0300

    :sparkles: feat: Create Back Button Component

---
+++ portsafe-frontend/src/components/BackButton.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { FaArrowLeft } from "react-icons/fa";
+
+interface BackButtonProps {
+  label?: string;
+  className?: string;
+}
+
+export default function BackButton({ label = "Voltar a Página Inicial", className }: BackButtonProps) {
+  const router = useRouter();
+
+  return (
+    <button
+      onClick={() => router.back()}
+      className={`flex items-center gap-2 text-base text-gray-400 hover:text-[#10ACD9] transition ${className || ""}`}
+    >
+      <FaArrowLeft size={18} />
+      {label}
+    </button>
+  );
+}
